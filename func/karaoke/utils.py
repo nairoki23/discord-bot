4fbb6a531a3c60d8a940eb3708876d9e599ad803
nairoki <nairoki.dev@gmail.com>
2026-01-02 13:45:19 +0900

    いらないので消した

--- func/karaoke/utils.py
+++ func/karaoke/utils.py
@@ -16,42 +16,6 @@ def t_process(title: str) -> str:
     title = re.sub(r"\s+", " ", title.strip())
     
     return title
-"""
-class Brand(IntEnum):
-    DAM=1
-    JOY=2
-@dataclass
-class RawSong:
-    title:str
-    artist:str
-    url:str
-    brand:Brand
-    exif:dict = field(default_factory=dict)
-
-@dataclass
-class Song:
-    title:str
-    artist:str
-    damUrl:str=""
-    joyUrl:str=""
-    exif:dict = field(default_factory=dict)
-
-@dataclass
-class RawArtist:
-    artist:str
-    code:int
-    brand:Brand
-    exif:dict = field(default_factory=dict)
-
-@dataclass
-class Artist:
-    artists: List[str]
-    damCode:list=field(default_factory=list)
-    joyCode:list=field(default_factory=list)
-    exif:dict = field(default_factory=dict)
-"""
-
-
 
 def hira_to_kata(text: str) -> str:
     """
